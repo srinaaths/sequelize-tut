567321589f2603a14d4ae56836be198972c8a0d9
srinaaths <srinaath.s@surveysparrow.com>
2022-07-08 23:29:56 +0530

    2nd commit - commented function

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const sequelize = new Sequelize('sequelize-video', 'srinaaths', '', {
     dialect: 'postgres'
 })
 
-const myFunc = async () => {
-    await sequelize.authenticate();
-    console.log('connected');
-}
+// const myFunc = async () => {
+//     await sequelize.authenticate();
+//     console.log('connected');
+// }
 
 sequelize.authenticate()
 .then(() => console.log('connected'))
